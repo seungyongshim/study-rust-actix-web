41da64b7a2feeddad10ed578d30be2b30c850f95
심승용 <seungyongshim@nexon.co.kr>
2023-11-23 19:44:01 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 extern crate actix_web;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpServer, Responder, web};
 use std::io::{*};
 
+mod hello_world;
+mod health_check;
 
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     HttpServer::new(|| {
-        App::new().route("/", web::get().to(hello_world))
+        App::new()
+            .route("/", web::get().to(hello_world::hello_world))
+            .route("/healthz", web::get().to(health_check::health_check))
     }).bind("127.0.0.1:8800")?
       .run().await
 }
 
-async fn hello_world() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
-}
+
